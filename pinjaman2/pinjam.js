3e01b253641c9f29fce02ba9058086adb54372c2
imamaccord12345 <imamaccord12345.imamaccord12345@gmail.com>
2021-08-01 00:39:48 +0700

    project pertama

---
+++ pinjaman2/pinjam.js
@@ -0,0 +1,19 @@
+// function a(){
+//     var tot=parseInt(document.getElementById('tot').value);
+//     var bunga=parseInt(document.getElementById('bunga').value);
+//     // var durasi=parseInt(document.getElementById('durasi').value);
+//     var persen=(tot*bunga)/100;
+//     var hasil=tot*persen;
+//     document.getElementById('hasil').innerHTML=hasil;
+// }
+
+
+function a(){
+    var tot=parseInt(document.getElementById('tot').value);
+    var bunga=parseInt(document.getElementById('bunga').value);
+    // var durasi=parseInt(document.getElementById('durasi').value);
+    var persen=(tot/1000000);
+    var hasil=persen*150000;
+    document.getElementById('hasil').innerHTML=hasil;
+}
+
